cf039ca43aecb78385cd05b25f6ab1d15293b75e
Dương Việt Anh <greenknights.anhvd@gmail.com>
2024-01-16 10:37:53 +0700

    Test

---
+++ Assets/Base/00_BaseCode/Scripts/Test_1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_1 : MonoBehaviour
+{
+    public static Test_1 Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+    public void HandleMethod()
+    {
+        Debug.LogError("HandleMethod");
+    }    
+}
---
+++ Assets/Base/00_BaseCode/Scripts/Test_2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_2 : MonoBehaviour
+{
+    
+
+    public void Start()
+    {
+        Test_1.Instance.HandleMethod();
+    }
+}
